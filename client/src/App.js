9011a780b02c0fcd9aed1fb60f555347c3b41bbb
Vincent MUCCHIELLI <vimucchi@e1r5p2.42.fr>
2019-08-12 17:49:19 +0200

    docker up

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './style/App.css';
 
 function App(){
-    return('done');
+    return('done Client side');
   }
   
-
 export default App;
--- server/App.js
+++ server/App.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/login', (req, res) => {
-  console.log("test")
-  res.Header('Access-Control-Allow-Origin','*')
-  
-  res.send('put')
+  console.log("test-backend")  
+  res.send('SalutServeur')
   console.log(res);
 });
 
