caeaf50c4fde99d069aa6025dcb44a960a32458d
Felix DECOMBAT <fedecomb@e1r6p1.42.fr>
2019-08-12 21:18:38 +0200

    post

--- server/App.js
+++ server/App.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -9,7 +10,8 @@ app.use((req, res, next) => {
 
 app.post('/login', (req, res) => {
   console.log("test-backend")  
-  res.send('SalutServeur')
+  var test = JSON.stringify({test : "AHHAHAHAHA"})
+  res.send(test)
   console.log(res);
 });
 
