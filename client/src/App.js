cc7f2b2d65c262bf664c1ffa759d0214ac7290fb
Felix DECOMBAT <fedecomb@e1r1p13.42.fr>
2019-08-27 11:14:40 +0200

    maj

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,7 @@
-// https://knowbody.github.io/react-router-docs/api/RouteComponents.html
-// https://auth0.com/blog/react-tutorial-building-and-securing-your-first-app/
-
 import './Assets/App.css';
 import React from "react";
 // import ReactDOM from "react-dom";
-
 import  AppRouter  from "./Services/MasterPage";
-
-
-const useStateWithLocalStorage = localStorageKey => {
-    console.log(localStorage);
-  const [value, setValue] = React.useState(
-    localStorage.getItem(localStorageKey) || ''
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(localStorageKey, value);
-  }, [value]);
-
-  return [value, setValue];
-};
-
-const App = () => {
-  const [value, setValue] = useStateWithLocalStorage(
-    'myValueInLocalStorage'
-  );
-
-  const onChange = event => setValue(event.target.value);
-
-  return (
-    <div>
-      <h1>Hello React with Local Storage!</h1>
-
-      <input value={value} type="text" onChange={onChange} />
-
-      <p>{value}</p>
-    </div>
-  );
-};
-
-
 
 function App(){
         return (
@@ -50,3 +12,34 @@ function App(){
 }
 
 export default App;
+
+// const useStateWithLocalStorage = localStorageKey => {
+//     console.log(localStorage);
+//   const [value, setValue] = React.useState(
+//     localStorage.getItem(localStorageKey) || ''
+//   );
+
+//   React.useEffect(() => {
+//     localStorage.setItem(localStorageKey, value);
+//   }, [value]);
+
+//   return [value, setValue];
+// };
+
+// const App = () => {
+//   const [value, setValue] = useStateWithLocalStorage(
+//     'myValueInLocalStorage'
+//   );
+
+//   const onChange = event => setValue(event.target.value);
+
+//   return (
+//     <div>
+//       <h1>Hello React with Local Storage!</h1>
+
+//       <input value={value} type="text" onChange={onChange} />
+
+//       <p>{value}</p>
+//     </div>
+//   );
+// };
--- server/App.js
+++ server/App.js
@@ -21,70 +21,6 @@ app.use(bodyParser.json());
 app.use('/registration', Registration);
 
 
-// recuperée un post client
-// async function whoisit(){
-//   await app.post('/newuser', (req, res, next) => {
-//   return console.log(req.body);
-//   })
-// }
-// whoisit().catch(err => console.log(err.message));
-
-// recuperere un post client
-async function whoisit() {
-  await app.post('/newuser', (req, res, next) => {
-  const data = req.body;
-  console.log(data);
-  console.log(data.user);
-    async function createData(dgraphClient, data) {
-      // Create a new transaction.
-      const txn = dgraphClient.newTxn();
-      try {
-          // Create data.
-          const p = data;
-
-          // Run mutation.
-          const mu = new dgraph.Mutation();
-          mu.setSetJson(p);
-          const assigned = await txn.mutate(mu);
-
-          // Commit transaction.
-          await txn.commit();
-
-          // Get uid of the outermost object (person named "Alice").
-          // Assigned#getUidsMap() returns a map from blank node names to uids.
-          // For a json mutation, blank node names "blank-0", "blank-1", ... are used
-          // for all the created nodes.
-          
-      } finally {
-          // Clean up. Calling this after txn.commit() is a no-op
-          // and hence safe.
-          await txn.discard();
-      }
-  }
-  createData(db.newClient(db.newClientStub()), data.user);
-return data; 
-})
-}
-
-whoisit().catch(err => console.log(err.message));
-
-
-
-// * Post serveur->client
-
-// app.post('/login', (req, res) => {
-//   async function makePostRequest() {
-//     const params = {
-//       id: 6,
-//       first_name: 'Fred',
-//       last_name: 'Blair',
-//       email: 'freddyb34@gmail.com'
-//     }
-//     console.log(res.body);
-//     res.send(params);
-//   }
-//   makePostRequest();
-// })
 
 // * Mise en place db + query + post serveur -> client
 
@@ -106,24 +42,5 @@ async function createdb() {
 }
 
 createdb();
-
-// app.post('/login', (req, res) => {
-//   async function f() {
-//     //get info
-//     const dgraphClientStub = db.newClientStub();
-//     const dgraphClient = db.newClient(dgraphClientStub);
-//     await db.dropAll(dgraphClient);
-//     await db.setSchema(dgraphClient);
-//     await db.createData(dgraphClient);
-//     const json = await db.queryData(dgraphClient);
-//     res.send(json)
-//     dgraphClientStub.close();
-//   };
-//   f().then(() => {
-//     console.log('YEAH');
-//   }).catch((e) => {
-//     console.log('NOOOO ! : ', e);
-//   });
-// });
 
 app.listen(8000, () => console.log('Example app listening on port 8000!'));
