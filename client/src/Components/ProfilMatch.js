8cf2cb9b79061780bc971411975718190ed02120
Vincent MUCCHIELLI <vimucchi@e1r4p20.42.fr>
2019-09-04 19:50:45 +0200

    minor changes

--- client/src/Components/ProfilMatch.js
+++ client/src/Components/ProfilMatch.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import axios from '../Services/Client';
 
-const profil = { 
-    username: "Herta_Swift74",
-    firstname: "Herta",
-    lastname: "Swift",
-    user_pic: "https://s3.amazonaws.com/uifaces/faces/twitter/jydesign/128.jpg"
-};
-
 class ProfilMatch extends React.Component {
+
     state = {
-        username: 'test',
+        username: '',
         firstname: '',
         user_pic: '',
     };
+
+    newUser = axios.post('/getUser').then((res) => {
+        this.setState({username:res.data.username, firstname:res.data.firstname, user_pic:res.data.user_pic});
+    });
 
     handleUser = async (event) => {
         event.preventDefault();
@@ -27,8 +25,9 @@ class ProfilMatch extends React.Component {
             <div className="Bloc">
                <form onSubmit = {this.handleUser}>
                     {this.state.username}
-                    <img src={this.state.user_pic}></img>
+                    <img alt="user-pic" src={this.state.user_pic}></img>
                     <p>Say Hello to {this.state.firstname}</p>
+                    <input type="submit" value="Match"></input>
                     <input type="submit" value="New Profile"></input>
                 </form>
             </div>
