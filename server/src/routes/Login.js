dc30fecd53ab5598d8b1e3790da45edbc4786d97
vincent2021 <vincent2021@gmail.com>
2019-09-10 12:42:26 +0200

    API in progress

--- server/src/routes/Login.js
+++ server/src/routes/Login.js
@@ -5,24 +5,19 @@ router.route("/connect").post((req, res) => {
     console.log(req.body);
     db.checkUser(req.body);
     //fonction pour verifier les nvx
-    res.send('alright !');
+    res.send('Login done');
 });
-
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-async function checkUser(username, password) {
-    //... fetch user from a db etc.
-    const match = await bcrypt.compare(password, user.passwordHash);
-    if (match) {
-        //login
-    }
-    //...
-}
 
 router.route("/register").post((req, res) => {
-    res.send(req.body);
+    console.log(req.body);
+    db.addUser(req.body);
+    res.send("User added to the db");
 });
 
+
+router.route("/userid").post((req, res) => {
+    const userID = "0x2";
+    res.send(userID);
+});
 
 module.exports = router;
