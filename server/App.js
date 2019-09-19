995c86a9bfee63f54c8567fc8d8973385a96c97e
Vincent MUCCHIELLI <vimucchi@e1r12p7.42.fr>
2019-09-19 15:25:42 +0200

    socket in progress

--- server/App.js
+++ server/App.js
@@ -10,11 +10,16 @@ const helmet = require('helmet'); // securise Express
 const bodyParser = require('body-parser'); // permet d'afficher le req.body apres une requête
 
 const app = express();
-const server = require('http').Server(app);
+
+
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.route("/socket.io").get((req, res) => {
+  res.send({ response: "I am alive" }).status(200);
+});
 app.use('/login', Login);
 app.use('/user', User);
 app.use('/match', Match);
@@ -22,8 +27,10 @@ app.use('/match', Match);
 
 app.listen(8000, () => console.log('Server listening on port 8000!'));
 
+
 //Mise en place du socket  pour le chat et notification
-const io = require('socket.io').listen(server); // Socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server, { origins: 'localhost:3000'});
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
