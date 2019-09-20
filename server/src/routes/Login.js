488867bdc4d3237ee948a3f45b2f335fcdca50dd
vincent2021 <vincent2021@gmail.com>
2019-09-20 14:18:29 +0200

    token info request

--- server/src/routes/Login.js
+++ server/src/routes/Login.js
@@ -17,11 +17,15 @@ router.route("/register").post((req, res) => {
     res.send("User added to the db");
 });
 
-
 router.route("/userid").post((req, res) => {
     db.getUserID(req.query['email']).then(function (ret) {
         res.send(ret);
     }, (err) => {console.log(err)});
+});
+
+router.route("/tokeninfo").post((req, res) => {
+    const tokenInfo = auth.decode(req.headers.authorization);
+    res.send(tokenInfo['payload']);
 });
 
 module.exports = router;
