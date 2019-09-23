72746c07c51ab1ea624c144eb6891dcf72a530d1
cobra-foutre <felixdec@hotmail.fr>
2019-09-23 15:46:49 +0200

    images

--- client/src/Components/Profil_User.js
+++ client/src/Components/Profil_User.js
@@ -14,7 +14,7 @@ const ProfilClient = () => {
         user_pic: []
     });
 
-    const [image, setImage] = useState({ pictures: [] });
+    const [images, setImage] = useState([]);
 
     if (user.username === '') {
         axios.post(`/login/tokeninfo`)
@@ -31,19 +31,53 @@ const ProfilClient = () => {
             })
     }
 
-    useEffect(() => {
-        let bloc = document.getElementById('BlocImage');
-        const img = document.createElement("img");
-        console.log(image.pictures[0]);
-        img.src = image.pictures[0];
-        bloc.appendChild(img)
-    }, [image])
+    // useEffect(() => {
+    //     let bloc = document.getElementById('BlocImage');
+    //     const img = document.createElement("img");
+    //     console.log(image.pictures[0]);
+    //     img.src = image.pictures[0];
+    //     bloc.appendChild(img)
+    // }, [image])
 
-    const ImportPicture = e => {
-        const fd = new FormData();
-        fd.append('image', e.target.files[0]);
-        setImage({ pictures: e.target.files });
+    const infos = () => {
+        console.log(images);
+        console.log(user);
     }
+
+    const convertPic = (pic) => {
+        return new Promise((res, rej) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(pic);
+            reader.onload = () => res(reader.result);
+            reader.onerror = error => rej(error);
+        })
+    }
+
+
+    const ImportPicture = async e => {
+        if (
+            e.target.files[0].type === "image/jpeg" ||
+            e.target.files[0].type === "image/png"
+        ) {
+            const img = await convertPic(e.target.files[0])
+
+            setImage([...images, img]);
+            setUser({ ...user, user_pic: images });
+
+            console.log(images, user)
+            const fd = new FormData();
+            fd.append('image', images);
+            axios.post(`/user/image`, fd)
+                .then(res => {
+                    console.log('reponses : ' + res.data);
+                })
+                .catch(err => {
+                    console.log('?' + err);
+                })
+
+        }
+    }
+
     const ModifyPicture = e => {
         console.log(e.target.value);
     }
@@ -69,6 +103,7 @@ const ProfilClient = () => {
                 <div className="BlocImport">
                     <input className="modify" onClick={ModifyPicture} type="submit" value="Modify Pics" style={{ backgroundColor: '#f99' }}></input>
                     <input id="ImportPicture" className="ImportPicture" onChange={ImportPicture} type="file" value=""></input>
+                    <button onClick={infos}>infos</button>
                     <label className="modify" htmlFor="ImportPicture" style={{ backgroundColor: '#ff3f' }} >Import Picture</label>
                 </div>
                 <div className="BlocInformations">
