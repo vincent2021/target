a88ec2604db58f4a2d9a5df9159e0bac03037708
Vincent MUCCHIELLI <vimucchi@e2r5p20.42.fr>
2019-09-30 17:22:37 +0200

    geologin

--- client/src/Services/Geo.js
+++ client/src/Services/Geo.js
@@ -4,17 +4,14 @@ const IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
 const GeolocationParams = require('ip-geolocation-api-javascript-sdk/GeolocationParams.js');
 
 export function getPos(token) {
-    const uid = decode(token).payload.uid;
-    const api_key = 'a8df2c6b5c02482baeae3fb17c70c98b';
-    const IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
-    const api = new IPGeolocationAPI(api_key, false); 
-    const params = new GeolocationParams(); 
-    params.setFields('city, latitude, longitude');
-    function handleResponse(json) {
-        console.log(json);
-        axios.post(`/user/setLocation?uid=${uid}`, json).then((res) => {
-            console.log(res);
-        })
-    }
-    return(api.getGeolocation(handleResponse, params));
+        const uid = decode(token).payload.uid;
+        const api_key = 'a8df2c6b5c02482baeae3fb17c70c98b';
+        const IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
+        const api = new IPGeolocationAPI(api_key, false); 
+        const params = new GeolocationParams(); 
+        params.setFields('city, latitude, longitude');
+        const handleResponse = (json) => {
+            axios.post(`/user/setLocation?uid=${uid}`, json).then((res) => {})
+        }
+        return(api.getGeolocation(handleResponse, params));
 }
