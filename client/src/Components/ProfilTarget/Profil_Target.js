a38549cf3ef4f450d31282b09fec9b54bda45197
Felix DECOMBAT <fedecomb@e1r10p21.42.fr>
2019-10-02 22:00:15 +0200

    photo container target fix + report + unlike

--- client/src/Components/ProfilTarget/Profil_Target.js
+++ client/src/Components/ProfilTarget/Profil_Target.js
@@ -16,11 +16,16 @@ const ProfilUser = ({ match }) => {
         interest: []
     });
     const [content, setContent] = useState('');
+    const [ImageContainer, setImageContainer] = useState('')
+
+    const SwapPic = e => {
+        e.preventDefault();
+        document.getElementById('ImageTarget').src = e.target.src;
+    }
 
     useEffect(() => {
         axios.post(`/user/profile?uid=${match.params.uid}`)
             .then(res => {
-                console.log(res.data);
                 setUser(res.data);
                 setAge(getAge(res.data.dob));
             })
@@ -30,57 +35,86 @@ const ProfilUser = ({ match }) => {
     }, [])
 
     const BlockUser = e => {
-        console.log(e.target.value);
+        e.preventDefault();
+        
     }
     const ReportUser = e => {
-        console.log(e.target.value);
+        e.preventDefault();
+        axios.post(`/user/reportuser?uid=`)
+        .then(res => {
+            console.log('report')
+        })
     }
     const UnlikeUser = e => {
-        console.log(e.target.value);
+        e.preventDefault();
+        axios.post(`/match/unlike?uid=${user.uid}`)
+            .then(res => {
+                console.log('unlike')
+            })
     }
     const ActiveChat = e => {
+        e.preventDefault();
         chatison === true ? setChatison(false) : setChatison(true);
         console.log(chatison);
     }
 
     useEffect(() => {
+        if (user.user_pic) {
+            document.getElementById('ImageTarget').src = user.user_pic[0];
+            setImageContainer(
+                user.user_pic.map((imgs, index) => (
+                    <img
+                        src={imgs}
+                        key={index}
+                        alt=""
+                        onClick={SwapPic}
+                    />)
+                )
+            );
+        }
+    }, [user]);
+
+    useEffect(() => {
         setContent(
             <div className="BlocBase">
-            <div className="BlocUser">
-                <div className="HeaderProfil">
-                    <p className="Titre">{user.username} / {Age}</p>
-                    <input id="BlockThisUser" onClick={BlockUser} type="submit" value="Block This User" style={{ backgroundColor: '#f55' }}></input>
-                    <input id="Report" onClick={ReportUser} type="submit" value="Report" style={{ backgroundColor: '#fb5' }}></input>
-                </div>
-                <div id="MatchBarre" className="MatchBarre"></div>
-                <span className="spanTarget">
-                    <img onLoad={e => { resizeImage(e, 300) }} id="ImageTarget" alt="profil" src={user.user_pic} className="imageTarget"></img>
-                </span>
-                <div id="BlocImage" className="BlocImage"></div>
-                <div className="BlocInformations">
-                    <p className="BlocTexte">
-                        Genre : {user.gender} <br />
-                        Interest in : {user.target} <br />
-                        Bio : {user.text} <br />
-                        Interest : {user.interest} <br />
-                        localistation: Moscou / Russia <br />
-                    </p >
+                <div className="BlocUser">
+                    <div className="HeaderProfil">
+                        <p className="Titre">{user.username} / {Age}</p>
+                        <input id="BlockThisUser" onClick={BlockUser} type="submit" value="Block This User" style={{ backgroundColor: '#f55' }}></input>
+                        <input id="Report" onClick={ReportUser} type="submit" value="Report" style={{ backgroundColor: '#fb5' }}></input>
+                    </div>
+                    <div id="MatchBarre" className="MatchBarre"></div>
+                    <span className="spanTarget">
+                        <img onLoad={e => { resizeImage(e, 300) }} id="ImageTarget" alt="profil" src='' className="imageTarget"></img>
+                    </span>
+                    <div className="BlocImage">
+                        {ImageContainer}
+                    </div>
+                    <div id="BlocImage" className="BlocImage"></div>
+                    <div className="BlocInformations">
+                        <p className="BlocTexte">
+                            Genre : {user.gender} <br />
+                            Interest in : {user.target} <br />
+                            Bio : {user.text} <br />
+                            Interest : {user.interest} <br />
+                            localistation: Moscou / Russia <br />
+                        </p >
+                    </div >
+                    <div className="UserButton">
+                        <input id="Unlike" onClick={UnlikeUser} type="submit" value="Unlike" style={{ backgroundColor: '#fcf' }}></input>
+                        <input id="Chat" onClick={ActiveChat} type="submit" value="Chat" style={{ backgroundColor: '#0b3b' }}></input>
+                    </div >
                 </div >
-                <div className="UserButton">
-                    <input id="Unlike" onClick={UnlikeUser} type="submit" value="Unlike" style={{ backgroundColor: '#fcf' }}></input>
-                    <input id="Chat" onClick={ActiveChat} type="submit" value="Chat" style={{ backgroundColor: '#0b3b' }}></input>
-                </div >
-            </div >
 
-            <div className="BlocUser">
-                <div className="BlocChat" id="BlocChat">
-                    <ChatBox chatison={chatison} setChatison={setChatison} />
-                </div >
-            </div>
-        </div >
+                <div className="BlocUser">
+                    <div className="BlocChat" id="BlocChat">
+                        <ChatBox chatison={chatison} setChatison={setChatison} />
+                    </div >
+                </div>
+            </div >
         )
 
-    }, [user.interest, user.text, user.target, user.gender])
+    }, [user.interest, user.text, user.target, user.gender, ImageContainer])
 
     return content
 }
